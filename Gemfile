d93ba161a47ee4013a35c3e682a414cd0cdf5dfd
kawata15 <kkw.bsk-r@outlook.com>
2019-02-05 19:17:39 +0900

    deviseの導入
    デバイスの導入
    userモデル作成（デバイスの初期カラムのみ）
    ルートの設定はしてないのでusers/sign_inとしないとログインページに飛ばない

--- Gemfile
+++ Gemfile
@@ -71,3 +71,4 @@ gem "font-awesome-rails"
 
 #font-awsome5対応
 gem 'font-awesome-sass'
+gem 'devise'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190201073000) do
+ActiveRecord::Schema.define(version: 20190205095114) do
 
   create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                                          null: false
@@ -35,6 +35,18 @@ ActiveRecord::Schema.define(version: 20190201073000) do
     t.index ["category_id"], name: "index_products_on_category_id", using: :btree
     t.index ["name"], name: "index_products_on_name", using: :btree
     t.index ["seller_id"], name: "index_products_on_seller_id", using: :btree
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
