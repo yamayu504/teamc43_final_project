1add3873432ad652d88f193fd7508cb551d5ba20
kawata15 <kkw.bsk-r@outlook.com>
2019-02-06 18:03:53 +0900

    カラムを追加したので仮想ユーザーのカラムも追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,13 +2,21 @@
 
 FactoryGirl.define do
   factory :user do
-    # nickname               "まいやん"
+    nickname               "まいやん"
     email                  "maiyan@email.com"
     password               "00001000"
     # password_confirmation  "00001000"
-    # first_name             "白石"
-    # last_name              "麻衣"
-    # last_name_kana         "シライシ"
-    # first_name_kana        "マイ"
+    first_name             "白石"
+    last_name              "麻衣"
+    last_name_kana         "シライシ"
+    first_name_kana        "マイ"
+    postal_code            "1234567"
+    # prefecture             "群馬県"
+    cities                 "沼田市"
+    address                "新町1801-1"
+    building_name          "乃木坂ビル46階"
+    birthday               "19920820"
+    profile                "四次元から来ましたマヨラー星人、19歳、まいやんこと、白石麻衣です"
+
   end
 end
