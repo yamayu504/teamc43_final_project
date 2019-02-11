2e9eb8636cf3d9fa17a1255ba20ec25442b46b4a
yamayu504 <yamayu.7349@gmail.com>
2019-02-11 20:38:52 +0900

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
+  get 'products/search' => 'products#search'
   resources   :products, only:[:new, :show, :create]
   get 'users' => 'users#index'
   get 'users/profile' => 'users#profile'
