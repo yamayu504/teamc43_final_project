75bf87d24e0b77cb9fdaf9b6d4c1e1e94ec342fb
yamayu504 <yamayu.7349@gmail.com>
2019-02-11 20:38:25 +0900

    Add create and search def

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,7 +10,38 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
   def create
-    redirect_to root_path
+    binding.pry
+    @product = Product.create!(create_params.merge(for_sale:1, deal: 0))
+    image_params[:p_images].each do |product_image|
+      binding.pry
+      @product.p_images.build
+      p_image = @product.p_images.new(image: product_image)
+      p_image.save
+    end
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+  def search
+    @brands = Brand.where('name LIKE(?)', "%#{params[:keyword]}%").limit(10)
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
+  private
+
+  def create_params
+      product_params = params.require(:product).permit(:name, :description,:category_id, :size, :brand_id, :condition, :select_shipping_fee, :shipping_method, :area, :shipping_date, :price).merge(seller_id: 1)
+      # 新規作成画面がないためseller_idは仮置き
+      return product_params
+  end
+  def image_params
+    binding.pry
+    params.require(:p_image).permit({:p_images => []})
+
   end
 
 end
