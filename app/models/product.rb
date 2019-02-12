4e794e72a2a70c30e7a3b15e9096e1c01131c01a
yamayu504 <yamayu.7349@gmail.com>
2019-02-12 19:42:59 +0900

    Add validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,10 +3,11 @@ class Product < ApplicationRecord
 
 has_many :product_images
 has_many :p_images, through: :product_images
-belongs_to :brand
+belongs_to :brand, optional: true
 belongs_to :category
 belongs_to :seller, class_name: "User"
 belongs_to :buyer,  class_name: "User", optional: true
+#like機能実装時に作成
 #has_many :likes
 
 enum condition:{"---": 0, "新品、未使用": 1, "目立った傷や汚れなし": 2, "やや傷や汚れあり": 3,"傷や汚れあり": 4, "全体的に状態が悪い": 5}
@@ -14,4 +15,11 @@ enum select_shipping_fee: {"---": 0, "送料込み(出品者負担)": 1, "着払
 enum shipping_method: {"---": 0,未定: 1, らくらくメルカリ便: 2, ゆうメール: 3, レターパック: 4,"普通郵便(定形、定形外)":5, クロネコヤマト: 6, ゆうパック: 7, クリックポスト:8, ゆうパケット:9},_prefix: true
 enum area: {"---": 0,北海道:1,青森県:2,岩手県:3,宮城県:4,秋田県:5,山形県:6,福島県:7,茨城県:8,栃木県:9,群馬県:10,埼玉県:11,千葉県:12,東京都:13,神奈川県:14,新潟県:15,富山県:16,石川県:17,福井県:18,山梨県:19,長野県:20,岐阜県:21,静岡県:22,愛知県:23,三重県:24,滋賀県:25,京都府:26,大阪府:27,兵庫県:28,奈良県:29,和歌山県:30,鳥取県:31,島根県:32,岡山県:33,広島県:34,山口県:35,徳島県:36,香川県:37,愛媛県:38,高知県:39,福岡県:40,佐賀県:41,長崎県:42,熊本県:43,大分県:44,宮崎県:45,鹿児島県:46,沖縄県:47},_prefix: true
 enum shipping_date:{"---": 0,"1日~2日で発送": 1,"2日~3日で発送": 2,"4日~7日で発送": 3},_prefix: true
+
+
+ validates :name, :description, :size, :condition, :select_shipping_fee, :shipping_method, :area, :shipping_date, :price, :category_id, presence: true
+validates :name, length: { maximum: 40 }
+validates :description, length: { maximum: 1000 }
+validates :price, numericality: { greater_than_or_equal_to: 300,less_than:10000000}
+validates :size, :condition, :select_shipping_fee, :shipping_method, :area, :shipping_date, numericality: { greater_than_or_equal_to: 1}, on: :create
 end
