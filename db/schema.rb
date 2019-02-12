63d2a22ddce5ff2e9980d3657fa0e64865d00431
yamayu504 <yamayu.7349@gmail.com>
2019-02-12 19:47:57 +0900

    Some fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,31 +10,76 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190206080807) do
+ActiveRecord::Schema.define(version: 20190212101903) do
+
+  create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.string   "group_name"
+    t.string   "name_initial"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",       null: false
+    t.integer  "parent_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "category_sizes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "category_id"
+    t.integer  "size_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["category_id"], name: "index_category_sizes_on_category_id", using: :btree
+    t.index ["size_id"], name: "index_category_sizes_on_size_id", using: :btree
+  end
+
+  create_table "p_images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "product_images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "product_id"
+    t.integer  "p_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["p_image_id"], name: "index_product_images_on_p_image_id", using: :btree
+    t.index ["product_id"], name: "index_product_images_on_product_id", using: :btree
+  end
 
   create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",                                          null: false
-    t.text     "description",         limit: 65535,             null: false
-    t.integer  "condition",                         default: 0, null: false
+    t.string   "name",                              default: "", null: false
+    t.text     "description",         limit: 65535,              null: false
+    t.integer  "condition",                         default: 0,  null: false
     t.integer  "size",                              default: 0
-    t.integer  "select_shipping_fee",               default: 0, null: false
-    t.integer  "shipping_method",                   default: 0, null: false
-    t.integer  "area",                              default: 0, null: false
-    t.integer  "shipping_date",                     default: 0, null: false
-    t.integer  "price",                                         null: false
-    t.integer  "seller_id"
+    t.integer  "select_shipping_fee",               default: 0,  null: false
+    t.integer  "shipping_method",                   default: 0,  null: false
+    t.integer  "area",                              default: 0,  null: false
+    t.integer  "shipping_date",                     default: 0,  null: false
+    t.integer  "price",                                          null: false
+    t.integer  "seller_id",                                      null: false
     t.integer  "buyer_id"
-    t.integer  "for_sale",                          default: 0, null: false
-    t.integer  "deal",                              default: 0, null: false
-    t.integer  "category_id",                                   null: false
+    t.integer  "for_sale",                          default: 0,  null: false
+    t.integer  "deal",                              default: 0,  null: false
+    t.integer  "category_id",                       default: 0,  null: false
     t.integer  "brand_id"
-    t.datetime "created_at",                                    null: false
-    t.datetime "updated_at",                                    null: false
+    t.datetime "created_at",                                     null: false
+    t.datetime "updated_at",                                     null: false
     t.index ["brand_id"], name: "index_products_on_brand_id", using: :btree
     t.index ["buyer_id"], name: "index_products_on_buyer_id", using: :btree
     t.index ["category_id"], name: "index_products_on_category_id", using: :btree
     t.index ["name"], name: "index_products_on_name", using: :btree
     t.index ["seller_id"], name: "index_products_on_seller_id", using: :btree
+  end
+
+  create_table "sizes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "size"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -63,4 +108,8 @@ ActiveRecord::Schema.define(version: 20190206080807) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "category_sizes", "categories"
+  add_foreign_key "category_sizes", "sizes"
+  add_foreign_key "product_images", "p_images"
+  add_foreign_key "product_images", "products"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.new(
+  nickname: 'まいやん',
+  email: "maiyan@email.com",
+  password: '00001000',
+  password_confirmation: '00001000',
+  first_name: '白石',
+  last_name: '麻衣',
+  last_name_kana: 'シライシ',
+  first_name_kana: 'マイ',
+  phone_number: '09011113333',
+  postal_code: '1234567',
+  prefecture: '0',
+  cities: '千代田区',
+  address: '新町',
+  building_name: '乃木坂ビル46階',
+  birthday: '19920820',
+  profile: '四次元から来ましたマヨラー星人、19歳、まいやんこと、白石麻衣です'
+  )
+user.save!
