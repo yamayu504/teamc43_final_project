3af2ebf7378d9c5d7ef3997794b1fe6301ae93c8
yamayu504 <yamayu.7349@gmail.com>
2019-02-13 12:31:24 +0900

    Change product_controller
    #what
    before_actionでif分の遷移を行うように変更しました。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  #updateメソッド時も利用？
+  before_action :image_confirmation, only: [:create]
   def index
   end
   def new
@@ -10,20 +12,14 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
   def create
-    if params[:p_image].present?
       @product = Product.create!(create_params.merge(for_sale:1, deal: 0))
-      image_params[:p_images].each do |product_image|
+      image_params[:p_images].each do |image|
         @product.p_images.build
-        p_image = @product.p_images.new(image: product_image)
-        p_image.save
+        @product.p_images.create(image: image)
       end
       respond_to do |format|
-        format.html
         format.json
       end
-    else
-      redirect_to new_product_path, alert: '入力に不備があります。必須項目を入力してください。'
-    end
   end
   def search
     @brands = Brand.where('name LIKE(?)', "%#{params[:keyword]}%").limit(10)
@@ -34,7 +30,6 @@ class ProductsController < ApplicationController
   end
 
   private
-
   def create_params
       product_params = params.require(:product).permit(:name, :description,:category_id, :size, :brand_id, :condition, :select_shipping_fee, :shipping_method, :area, :shipping_date, :price).merge(seller_id: 1)
       # 新規作成画面がないためseller_idは仮置き
@@ -42,7 +37,11 @@ class ProductsController < ApplicationController
   end
   def image_params
     params.require(:p_image).permit({:p_images => []})
-
   end
-
+  def image_confirmation
+    if params[:p_image].present?
+    else
+      redirect_to new_product_path, alert: '入力に不備があります。必須項目を入力してください。'
+    end
+  end
 end
