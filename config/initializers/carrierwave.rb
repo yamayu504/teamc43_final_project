2bb3e1a7937383efcf110f1f2399b42db3316120
yamayu504 <yamayu.7349@gmail.com>
2019-02-14 12:41:55 +0900

    Add local setting

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,6 +4,7 @@ require 'carrierwave/storage/fog'
 
 
 CarrierWave.configure do |config|
+  if Rails.env.production?
   config.storage         = :fog
   config.fog_provider    = 'fog/aws'
   config.fog_credentials = {
@@ -14,4 +15,5 @@ CarrierWave.configure do |config|
   }
   config.fog_directory   = 'freemarket-43c'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket-43c'
+  end
 end
