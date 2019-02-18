4bbea85925a156f011cc57ad3c3655c821acebd9
yamayu504 <yamayu.7349@gmail.com>
2019-02-18 20:32:54 +0900

    Fix routes and scss
    #what
    ルーティングとscssを更新しました。

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
 
   root 'products#index'
   get 'products/search' => 'products#search'
-  resources :products, only:[:new, :show, :create]
+  resources :p_images, only:[:destroy]
+  resources :products, only:[:new, :show, :create, :destroy,:edit,:update]
   resources :users, only:[:index] do
     resources :identifications, only:[:show,:index]
     resource  :identifications, only:[:update]
-    resources  :listings, only:[:index]
+    resources  :listings, only:[:index,:show,:update,]
     resources  :in_progresses, only:[:index]
     resources  :completeds, only:[:index]
     resources  :purchases, only:[:index]
