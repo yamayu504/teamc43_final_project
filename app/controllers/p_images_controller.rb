8abafca82c5e2fab87b0661d839616bf3f4b8f82
yamayu504 <yamayu.7349@gmail.com>
2019-02-18 20:32:20 +0900

    Add update and edit action for product and p_image
    #what
    Productの更新に当たって、画像の削除と商品の更新のためのバックエンド及び更新画面を作成した。

---
+++ app/controllers/p_images_controller.rb
@@ -0,0 +1,12 @@
+class PImagesController < ApplicationController
+
+  def destroy
+    p_image = PImage.find(params[:id])
+    binding.pry
+    product_seller = p_image.product.seller_id
+    p_image.destroy if product_seller == current_user.id
+    respond_to do |format|
+      format.json
+    end
+  end
+end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,4 @@
+
 class ProductsController < ApplicationController
   #updateメソッド時も利用？
   before_action :image_confirmation, only: [:create]
@@ -19,14 +20,37 @@ class ProductsController < ApplicationController
     @user_products =Product.where(seller_id: @product.seller_id).limit(6)
   end
   def create
-    @product = Product.create!(create_params.merge(for_sale:1, deal: 0))
-    image_params[:p_images].each do |image|
-      @product.p_images.build
-      product_image = @product.p_images.new(image: image)
-      product_image.save
+    @product = Product.new(create_params.merge(for_sale:1, deal: 0))
+    if @product.save
+      image_params[:p_images].each do |image|
+        @product.p_images.build
+        product_image = @product.p_images.new(image: image)
+        product_image.save
+      end
+      respond_to do |format|
+        format.json
+      end
     end
-    respond_to do |format|
-      format.json
+  end
+  def update
+    if params[:p_image].present?
+      @product = Product.find(params[:id])
+      if @product.update(create_params.merge(for_sale:1, deal: 0))
+        image_params[:p_images].each do |image|
+          @product.p_images.build
+          product_image = @product.p_images.new(image: image)
+          product_image.save
+        end
+        respond_to do |format|
+          format.json
+        end
+      end
+    else
+      @product = Product.find(params[:id])
+      @product.update(create_params.merge(for_sale:1, deal: 0))
+      respond_to do |format|
+        format.json
+      end
     end
   end
   def search
@@ -36,11 +60,20 @@ class ProductsController < ApplicationController
       format.json
     end
   end
+  def edit
+    @product    = Product.find(params[:id])
+    # カテゴリー機能作成まで借りで全て参照可能
+    @categories = Category.all
+    @sizes      = Size.all
+    @brands     = Brand.all
+  end
+
+
   private
   def create_params
       product_params = params.require(:product).permit(:name, :description,:category_id, :size, :brand_id, :condition, :select_shipping_fee, :shipping_method, :area, :shipping_date, :price).merge(seller_id: 1)
       # 新規作成画面がないためseller_idは仮置き
-      product_params[:brand_id] = Brand.find_by(name: product_params[:brand_id]).id
+      product_params[:brand_id] = Brand.find_by(name: product_params[:brand_id]).id if product_params[:brand_id].present?
       return product_params
   end
   def image_params
