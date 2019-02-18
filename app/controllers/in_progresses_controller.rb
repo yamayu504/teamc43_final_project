66062b5406d57965e239bc77016f38aba93ff10c
yamayu504 <yamayu.7349@gmail.com>
2019-02-18 21:09:47 +0900

    Remove unnecessary code
    #what
    空行と改行忘れを修正しました。

--- app/controllers/in_progresses_controller.rb
+++ app/controllers/in_progresses_controller.rb
@@ -1,6 +1,5 @@
 class InProgressesController < ApplicationController
   def index
-    @products = current_user.sell_products.where.not(buyer_id: nil, deal:1
-      )
+    @products = current_user.sell_products.where.not(buyer_id: nil, deal:1)
   end
 end
