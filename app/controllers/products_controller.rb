11502d59ded2e3435082e44cf1b7fe472706c264
yamayu504 <yamayu.7349@gmail.com>
2019-02-19 14:39:07 +0900

    Add set_product
    #what
    set_productを作成し、@product    =
    Product.find(params[:id])をbefore_actionでまとめた。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,7 @@
 class ProductsController < ApplicationController
   #updateメソッド時も利用？
   before_action :image_confirmation, only: [:create]
+  before_action :set_product, only:[:show,:update, :edit, :destroy]
   def index
     brand_ids    = Product.group(:brand_id).order('count_brand_id DESC').limit(2).count(:brand_id).keys
     category_ids = Product.group(:category_id).order('count_category_id DESC').limit(2).count(:category_id).keys
@@ -14,7 +15,6 @@ class ProductsController < ApplicationController
     @choices = {"---": 0, "レディース": 1,"メンズ":2}
   end
   def show
-    @product = Product.find(params[:id])
     @images  = @product.p_images
     @similer_categories  = Product.where(category_id: @product.category_id).order('id ASC').limit(6)
     @user_products =Product.where(seller_id: @product.seller_id).limit(6)
@@ -34,7 +34,6 @@ class ProductsController < ApplicationController
   end
   def update
     if params[:p_image].present?
-      @product = Product.find(params[:id])
       if @product.update(create_params.merge(for_sale:1, deal: 0))
         image_params[:p_images].each do |image|
           @product.p_images.build
@@ -46,7 +45,6 @@ class ProductsController < ApplicationController
         end
       end
     else
-      @product = Product.find(params[:id])
       @product.update(create_params.merge(for_sale:1, deal: 0))
       respond_to do |format|
         format.json
@@ -61,16 +59,14 @@ class ProductsController < ApplicationController
     end
   end
   def edit
-    @product    = Product.find(params[:id])
     # カテゴリー機能作成まで借りで全て参照可能
     @categories = Category.all
     @sizes      = Size.all
     @brands     = Brand.all
   end
   def destroy
-    product = Product.find(params[:id])
-      product.destroy if product.seller.id == current_user.id
-      redirect_to user_listings_path(user_id: current_user.id), notice: "商品が削除されました。"
+    @product.destroy if product.seller.id == current_user.id
+    redirect_to user_listings_path(current_user), notice: "商品が削除されました。"
   end
 
   private
@@ -88,5 +84,8 @@ class ProductsController < ApplicationController
     else
       redirect_to new_product_path, alert: '入力に不備があります。必須項目を入力してください。'
     end
+  end
+  def set_product
+    @product    = Product.find(params[:id])
   end
 end
