c54e05fec85310e45ac69fdbdcd15cd8f6607a0a
genki kitamura <www.08056561815@gmail.com>
2019-02-21 18:52:40 +0900

    Merge branch 'yamayu504/master' into add_-Identification

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -2,7 +2,7 @@ class BuysController < ApplicationController
   before_action :sign_in_confirmation
   def index
     @product = Product.find(params[:product_id])
-    if @product.seller_id == current_user.id
+    unless @product.seller_id == current_user.id
     else
       redirect_to root_path
     end
@@ -10,10 +10,18 @@ class BuysController < ApplicationController
   def update
     @product = Product.find(params[:product_id])
     if @product.seller_id == current_user.id
-      redirect_to root_path
+      redirect_to root_path, notice: "ご自身が出品した商品は買えません"
     else
       @product.update(deal: 1, buyer_id: current_user.id)
       @product.save
+    # pay.jp側に売上として反映させる
+    Payjp.api_key = PAYJP_SECRET_KEY
+    Payjp::Charge.create(
+      amount: @product.price,
+      customer: current_user.customer_id,
+      currency: 'jpy',
+    )
+      redirect_to root_path, notice: "商品を購入しました"
     end
   end
 
